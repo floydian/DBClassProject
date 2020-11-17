18f81289ac99d1b1a146588255e5e1dc7c703292
ehmuller <74625943+ehmuller@users.noreply.github.com>
2020-11-17 16:49:15 -0500

    Update db.php

--- htdocs/includes/db.php
+++ htdocs/includes/db.php
@@ -1,7 +1,27 @@
 <?php
-
+  
 /*
   The db class file
   Database user: db_project_user
   Database pass: db_project_pass
 */
+  
+$username = $_POST["user"];
+$password = $_POST["pass"];
+
+function db_connect() {
+$conn = mysqli_connect("localhost", "root", "","db_Class_project");
+if (!$conn) {
+   throw new Exception("Failed to Connect: " . mysqli_connect_error());
+	     }
+return $conn;
+}
+
+
+try{
+	$conn = db_connect();
+    } catch (Exception $e) {
+	echo 'Caught Exception: ', $e->getMessage(), "\n";
+	}	
+
+?>
