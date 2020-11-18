4b1fea95bbecad38c3e0dd0e434e674c8bf6fc6f
Wayne Fincher <wayne.fincher@live.com>
2020-11-17 19:05:07 -0500

    Update gym.php

--- htdocs/pages/gym.php
+++ htdocs/pages/gym.php
@@ -1,17 +1,17 @@
 <?php
 
+set_page_title("Gym");
+$training_result = '';
+
 try {
   $dbh = db_connect();
 } catch (Exception $error) {
-  return; // die();
+  set_page_body("Sorry, but something went wrong. Please check back later.");
+  return; 
 }
 
-set_page_title("Gym");
 
-
-$user = user_get();
-
-if (!$user) {
+if (! ($user = user_get()) ) {
 	
 	set_error_message(<<<EOT
 You're not logged in. Please login in here: <a href="index.php?p=login">Login</a>
@@ -22,17 +22,37 @@ EOT
 }
 
 
+
 $train_type = $_REQUEST['train_type'] ?? null;
 
+// if (!is_null($train_type) || !in_array($train_type, ['strength', 'defense', 'agility' /*, .... */]) ) {
+
 if (!is_null($train_type)) {
-	// process the training.
+  if ($train_type == 'strength') {
+	  // update user set energy = energy - 1 where userid = ?
+	  // check and see that the energy was actually taken.
+	  // If not, tell user
+	  if ($error) {
+		  $training_result = 'sorry, but you are out of energy'; // return;
+	  } else {
+		// train strength, by executing sql (update user_stat set strength = strength + 1 where userid = ? )
+		  // select...
+		 $training_result = 'you now have strength: ?'; // return;
+	  }
+	  
+  }
+      
+      // handle agility training
 }
 
 // Whether someone is training or not, reshow the gym training form (which I haven't added to the set_page_body() function call).
 
 set_page_body(<<<EOT
-Train strength<br>
+<form method="post" action="index.php?p=gym">
+<input type="button" name="train_type" value="strength" Train strength<br>
+<input type="button" name="train_type" value="agility" Train agility<br>
 Train defense<br>
+$training_result
 EOT
 );
 
