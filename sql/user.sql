5c920db954aafd8f742197cdb210c2a59105a4f9
Wayne Fincher <wayne.fincher@live.com>
2020-11-18 18:07:06 -0500

    Added an email attribute

--- sql/user.sql
+++ sql/user.sql
@@ -1,12 +1,14 @@
-CREATE TABLE `db_class_project`.`user` (
-  `userid` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `display_name` VARCHAR(30) NULL,
-  `login_name` VARCHAR(30) NOT NULL,
-  `password` VARCHAR(97) NOT NULL,
-  `join_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_login` DATETIME NULL,
-  `premium_status` DATETIME NULL,
+CREATE TABLE `user` (
+  `userid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `display_name` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `login_name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(97) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `join_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `last_login` datetime DEFAULT NULL,
+  `premium_status` datetime DEFAULT NULL,
+  `email` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`userid`),
-  UNIQUE INDEX `display_name_UNIQUE` (`display_name` ASC),
-  UNIQUE INDEX `login_name_UNIQUE` (`login_name` ASC));
-  
+  UNIQUE KEY `login_name_UNIQUE` (`login_name`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `display_name_UNIQUE` (`display_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
