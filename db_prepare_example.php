d77523db44944e4bc7ae4993a13b8658b95e1ed8
Wayne Fincher <wayne.fincher@live.com>
2020-11-18 00:28:49 -0500

    Update db_prepare_example.php

--- db_prepare_example.php
+++ db_prepare_example.php
@@ -1,17 +1,72 @@
 <?php
 
+$error = false;
 
-			$stmt =  $conn->stmt_init();
-			if ( !$stmt->prepare('insert into user (login_name, password) values (?, ?)') ) {
-				$create_errors |= self::CANT_CREATE_USER;
-			} else if ( !$stmt->bind_param('ss', $config['login_name'], self::passHash($config['password'])) ) {
-				$create_errors |= self::CANT_CREATE_USER;
-			} else if (!$stmt->execute()) {
-				$create_errors |= self::CANT_CREATE_USER;
-			} else if ( !($result = $stmt->get_result()) ) {
-				$create_errors |= self::CANT_CREATE_USER;
-			} else if ( !($data = $result->fetch_array(MYSQLI_ASSOC)) ) {
-				$create_errors |= self::CANT_CREATE_USER;
-			} else {
-				$stmt->close();
-			}
+// suitable when selecting one row from the database.
+$stmt =  $conn->stmt_init();
+if ( !$stmt->prepare('some select query') ) {
+	$error = true;
+} else if ( !$stmt->bind_param('ss', $param_1, $param_2) ) {
+	$error = true;
+} else if (!$stmt->execute()) {
+	$error = true;
+} else if ( !($result = $stmt->get_result()) ) {
+	$error = true;
+} else if ( !($data = $result->fetch_array(MYSQLI_ASSOC)) ) {
+	$error = true;
+} else {
+	/*
+		Success.
+		Do some successful things here.
+	*/
+}
+$stmt->close();
+
+
+
+
+// This next example is suitable for when there are multiple rows returned
+// from the select statement.
+
+$error = false;
+
+// suitable when selecting one row from the database.
+$stmt =  $conn->stmt_init();
+if ( !$stmt->prepare('some select query') ) {
+	$error = true;
+} else if ( !$stmt->bind_param('ss', $param_1, $param_2) ) {
+	$error = true;
+} else if (!$stmt->execute()) {
+	$error = true;
+} else if ( !($result = $stmt->get_result()) ) {
+	$error = true;
+} else {
+	
+	// Be careful. If your query returned an empty result set and if you need to know that,
+	// then use a counter in the while loop.
+	$num_results = 0;
+	while($data = $result->fetch_array(MYSQLI_ASSOC)) {
+		$num_results++;
+		// save or process data
+	}
+	
+	if ($num_results > 0) {
+		/*
+			Success.
+			Do some successful things here.
+		*/
+	}
+}
+$stmt->close();
+
+
+
+
+
+
+
+
+
+
+
+
