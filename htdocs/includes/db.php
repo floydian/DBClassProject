b7242aaf9d8a6f22d3c2358b1f183c891c46ddde
Wayne Fincher <wayne.fincher@live.com>
2020-11-17 23:56:21 -0500

    Added a small static class
    The static class aids in accessing a reference to the database connection variable.

--- htdocs/includes/db.php
+++ htdocs/includes/db.php
@@ -6,13 +6,28 @@
   Database pass: db_project_pass
 */
 
+// This small class is intended to prevent a problem where a
+// reference to the database connection is needed in an included script
+// but the included script has no idea what the name of the variable
+// containing the database reference is in the global namespace.
+// Hence, we store $conn in DB::$conn which is accessible inside of any
+// scope. You can still do $dbh = db_connect(); as before.
+// But now, once db_connect() is called, DB::$conn will be available as well.
+class DB {
+	static public $conn;
+}
+
+
+
 function db_connect() {
-$conn = mysqli_connect("localhost", "root", "","db_Class_project");
+DB::$conn = mysqli_connect("localhost", "db_project_user", "db_project_pass","db_class_project");
 if (!$conn) {
    throw new Exception("Failed to Connect: " . mysqli_connect_error());
 	     }
-return $conn;
+return DB::$conn;
 }
+
+
 
 /*
 try{
@@ -24,6 +39,3 @@ try{
 	echo 'Caught Exception: ', $e->getMessage(), "\n";
 	}
 */	
-
-
-// down here
