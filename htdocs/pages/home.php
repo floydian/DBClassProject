ba77e0a5f7290913237977d33108fe96b8d9d6e2
Wayne Fincher <slowlychillin@yahoo.com>
2020-11-19 08:22:55 -0500

    Added the home page

---
+++ htdocs/pages/home.php
@@ -0,0 +1,36 @@
+<?php
+
+set_page_title("Home");
+
+try {
+  $dbh = db_connect();
+} catch (Exception $error) {
+  set_page_body("Sorry, but something went wrong. Please check back later.");
+  return; 
+}
+
+try {
+	$user = User::load();
+} catch (Exception $error) {
+	require('login.php');
+	return;
+}
+
+set_page_body(<<<EOT
+Home page for the game.
+EOT
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
